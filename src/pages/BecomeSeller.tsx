b902d7f2fc3796000c6719a4b27409b339a6a7ed
chaudharyakshat239 <chaudharyakshat239@gmail.com>
2026-01-09 03:08:42 +0530

    "updated"

--- src/pages/BecomeSeller.tsx
+++ src/pages/BecomeSeller.tsx
@@ -37,7 +37,7 @@ export default function BecomeSeller() {
         description: "Welcome to the seller dashboard",
       });
 
-      navigate("/seller");
+      navigate("/seller/SellerDashboard");
     } catch (err: unknown) {
   const message =
     err instanceof Error ? err.message : "Something went wrong";
