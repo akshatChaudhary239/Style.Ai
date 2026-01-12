3ead59d419990ea597f65043c1824bceb1bdb3e1
chaudharyakshat239 <chaudharyakshat239@gmail.com>
2026-01-13 01:44:55 +0530

    "updated"

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -15,7 +15,7 @@ const NotFound = () => {
         <h1 className="mb-4 text-4xl font-bold">404</h1>
         <p className="mb-4 text-2xl text-muted-foreground">Oops! Page not found</p>
         <a href="/" className="text-primary underline hover:text-primary/90">
-          Return to Home
+          
         </a>
         <button onClick={() => navigate("/buyer")}>Go to Dashboard</button>
 
