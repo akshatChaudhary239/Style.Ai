458de3147d558077af027f8ac6735aa6deac441a
chaudharyakshat239 <chaudharyakshat239@gmail.com>
2026-01-23 19:43:34 +0530

    "updated"

--- src/components/ProfileForm.tsx
+++ src/components/ProfileForm.tsx
@@ -312,7 +312,7 @@ return (
           </div>
         </motion.div>
 
-        {/* Section: Body Types */}
+        {/* Section: Body Type */}
         <motion.div
           initial={{ opacity: 0, y: 10 }}
           animate={{ opacity: 1, y: 0 }}
